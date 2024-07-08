f6e4b192d583e7de1671a794267f9a147d6736b1
Stefano Borini <stefano.borini@gmail.com>
2024-07-08 19:36:55 +0100

    Trying with some random values

--- src/converter.c
+++ src/converter.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main() {
+    srand(time(0));
     double value = (rand() % 100 / 100.0);
     printf("%g\n", value);
     return 0;
