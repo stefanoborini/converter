78e00121271c12e2c8fc69201c5815eec583f42d
Stefano Borini <stefano.borini@gmail.com>
2024-07-08 19:48:43 +0100

    Verifying if the sum of two uniform dists is not uniform

--- src/converter.c
+++ src/converter.c
@@ -3,7 +3,8 @@
 #include <uxhw.h>
 
 int main() {
-    double value = UxHwDoubleUniformDist(0.5, 1.0);
-    printf("%g\n", value);
+    double value1 = UxHwDoubleUniformDist(0.5, 1.0);
+    double value2 = UxHwDoubleUniformDist(0.5, 1.0);
+    printf("%g\n", value1 + value2);
     return 0;
 }
