21ca5a528ff31b40be46ffe2ef572aa41adc4524
Stefano Borini <stefano.borini@gmail.com>
2024-07-08 19:59:41 +0100

    Do I still remember how to parse stuff with scanf?

--- src/converter.c
+++ src/converter.c
@@ -2,9 +2,16 @@
 #include <stdlib.h>
 #include <uxhw.h>
 
-int main() {
-    double value1 = UxHwDoubleUniformDist(0.5, 1.0);
-    double value2 = UxHwDoubleUniformDist(0.5, 1.0);
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        return 1;
+    }
+    double high;
+
+    sscanf(argv[1], "%f", &high);
+
+    double value1 = UxHwDoubleUniformDist(0.5, high);
+    double value2 = UxHwDoubleUniformDist(0.5, high);
     printf("%g\n", value1 + value2);
     return 0;
 }
